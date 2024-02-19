0c9ef0bd0b52487d49203c2da1c3352d8b706705
DJ Passey <djpasseyjr@gmail.com>
2024-02-19 16:08:04 -0500

    typo fix

---
+++ experiments/exp1/single_slurm_job.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+module purge
+module load python/3.9.6
+pip install sktime torch pysindy
+pip install /nas/longleaf/home/djpassey/interfere/
+
+python /nas/longleaf/home/djpassey/interfere/experiments/exp1/generate_data.py $1
