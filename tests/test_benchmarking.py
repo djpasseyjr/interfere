0f4087a142691bb315744be81500e6baed3a0985
DJ Passey <djpasseyjr@gmail.com>
2024-02-29 14:03:14 -0500

    updated tests

--- tests/test_benchmarking.py
+++ tests/test_benchmarking.py
@@ -20,14 +20,14 @@ def test_rmsse():
 
 
 def test_directional():
-    X_train = np.random.rand(20, 4)
-    X_true = np.random.rand(10, 4)
-    X_false = np.zeros((10, 4))
+    X_train = np.random.rand(20, 10)
+    X_true = np.random.rand(10, 10)
+    X_false = np.zeros((10, 10))
     X_pred_good = X_true
     intervention_idxs = np.array([0])
 
-    rmsse = DirectionalChangeBinary()
-    x_false_err = rmsse(X_train, X_true, X_false, intervention_idxs)
-    x_true_err = rmsse(X_train, X_true, X_pred_good, intervention_idxs)
+    dir_change = DirectionalChangeBinary()
+    x_false_err = dir_change(X_train, X_true, X_false, intervention_idxs)
+    x_true_err = dir_change(X_train, X_true, X_pred_good, intervention_idxs)
     assert x_false_err < x_true_err
 
