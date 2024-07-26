8a6f2e6750b49550f7ac7455319e7388d879322c
DJ Passey <djpasseyjr@gmail.com>
2024-07-26 10:06:46 -0400

    moved experiments to a new repo

--- experiments/exp1/runner/single_slurm_job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-module purge
-module load anaconda
-conda activate interfere
-
-python /nas/longleaf/home/djpassey/interfere/experiments/exp1/runner/run.py $1
--- experiments/exp2/runner/single_slurm_job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-module purge
-module load anaconda
-conda activate interfere_exp2
-
-python /nas/longleaf/home/djpassey/interfere/experiments/exp2/runner/run.py $1
